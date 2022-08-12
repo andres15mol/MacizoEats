d339dc4a902c7d5b8fbb22d1c4b7b3d9ea11ee63
andres15mol <andres15mol@gmail.com>
2022-08-12 09:59:39 -0600

    Version completa con node y backend
    Primera version completa

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var database = require("./modules/database");
+var bodyParser = require("body-parser");
+var usuariosRouter = require("./routers/usuarios-router");
+var app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use('/usuarios',usuariosRouter);
+
+
+app.listen(5050, function(){
+    console.log("Servidor en linea");
+});
